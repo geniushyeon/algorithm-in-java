12ddba9bcc187c78cda512dcca66e470d53e38b4
geniushyeon <jhlee0912@icloud.com>
2022-06-29 22:31:00 +0900

    ✨ [codewars] 모음의 개수

---
+++ src/codewars/kyu7/VowelCount.java
@@ -0,0 +1,24 @@
+package codewars.kyu7;
+
+public class VowelCount {
+
+	public static int getCount(String str) {
+		int vowelsCount = 0;
+
+		char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+
+		for (char c : str.toCharArray()) {
+			for (char v : vowels) {
+				if (c == v) {
+					vowelsCount++;
+				}
+			}
+		}
+		return vowelsCount;
+	}
+
+	public static void main(String[] args) {
+		String str = "abracadabra";
+		System.out.println(getCount(str));
+	}
+}
