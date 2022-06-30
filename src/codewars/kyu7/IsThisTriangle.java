3ba656b9cc6650c1fdd8d3bd45497976ad0ba7d9
geniushyeon <jhlee0912@icloud.com>
2022-06-30 21:40:57 +0900

    ✨ [codewars] 삼각형의 성립조건

---
+++ src/codewars/kyu7/IsThisTriangle.java
@@ -0,0 +1,11 @@
+package codewars.kyu7;
+
+public class IsThisTriangle {
+	public static boolean isTriangle(int a, int b, int c){
+		return a < b + c && b < a + c && c < a + b;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isTriangle(3, 3, 5));
+	}
+}
