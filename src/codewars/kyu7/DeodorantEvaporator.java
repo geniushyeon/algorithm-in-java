b5cfd81155f456a3c537bac1d70af3d60704984a
geniushyeon <jhlee0912@icloud.com>
2022-07-10 10:57:34 +0900

    ✨ [codewars] xo의 개수, deodorant evaporator

---
+++ src/codewars/kyu7/DeodorantEvaporator.java
@@ -0,0 +1,21 @@
+package codewars.kyu7;
+
+public class DeodorantEvaporator {
+
+	public static int evaporator(double content, double evap_per_day, double threshold) {
+
+		int result = 0;
+		double percentage = 100;
+
+		while (percentage > threshold) {
+			percentage -= percentage * (evap_per_day / 100);
+			result++;
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(evaporator(10, 10, 10));
+	}
+}
---
+++ src/codewars/kyu7/ExesAndOhs.java
@@ -0,0 +1,15 @@
+package codewars.kyu7;
+
+public class ExesAndOhs {
+
+	public static boolean getXO(String str) {
+		str = str.toLowerCase();
+
+		return str.replace("o", "").length() == str.replace("x", "").length();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getXO("XxxxooO"));
+		System.out.println(getXO("zssddd"));
+	}
+}
