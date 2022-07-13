72e78e285d599a30cd51091ebe6c5c02c4950f9d
geniushyeon <jhlee0912@icloud.com>
2022-07-13 22:37:17 +0900

    ✨ [codewars] 문자열 가리기, 남은 승객의 수

---
+++ src/codewars/kyu7/CreditCardMask.java
@@ -0,0 +1,36 @@
+package codewars.kyu7;
+
+public class CreditCardMask {
+
+	public static String maskify(String str) {
+
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 0; i < str.length() - 4; i++) {
+			result.append("#");
+		}
+
+		return result + str.substring(str.length() - 4);
+	}
+
+	public static String maskifyWithRegExp(String str) {
+		if (str.length() <= 4) {
+			return str;
+		}
+
+		String maskified = str.substring(0, str.length() - 4).replaceAll("[\\s\\w\\W]", "#");
+		String substring = str.substring(str.length() - 4);
+
+		return maskified + substring;
+	}
+
+	public static String clever(String str) {
+		return str.replaceAll(".(?=.{4})", "#");
+	}
+
+	public static void main(String[] args) {
+		System.out.println(maskify("4556364607935616"));
+		System.out.println(maskifyWithRegExp("4556364607935616"));
+		System.out.println(clever("4556364607935616"));
+	}
+}
---
+++ src/codewars/kyu7/NumberOfPeopleInTheBus.java
@@ -0,0 +1,36 @@
+package codewars.kyu7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberOfPeopleInTheBus {
+
+	public static int countPassengers(List<int[]> stops) {
+
+		int result = 0;
+
+		for (int[] stop : stops) {
+			result += stop[0] - stop[1];
+		}
+
+		return result;
+	}
+
+	public static int clever(List<int[]> stops) {
+		return stops.stream()
+				.mapToInt(x -> x[0] - x[1])
+				.sum();
+	}
+
+
+	public static void main(String[] args) {
+
+		List<int[]> stops = new ArrayList<>();
+		stops.add(new int[] {10, 0});
+		stops.add(new int[] {3, 5});
+		stops.add(new int[] {2, 5});
+
+		System.out.println(countPassengers(stops));
+		System.out.println(clever(stops));
+	}
+}
