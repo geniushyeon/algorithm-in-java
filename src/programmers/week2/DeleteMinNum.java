e79de6074bd90f529366e0b4ea2622a07a1a5e3e
geniushyeon <jhlee0912@icloud.com>
2022-07-25 20:43:06 +0900

    ♻️ [programmers] 제일 작은 수 제거하기 리팩토링

--- src/programmers/week2/DeleteMinNum.java
+++ src/programmers/week2/DeleteMinNum.java
@@ -1,35 +1,36 @@
 package programmers.week2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 // 제일 작은 수 제거하기
 public class DeleteMinNum {
     public static int[] solution(int[] arr) {
-        int[] answer = {};
+
+        if (arr.length == 1) {
+            return new int[] {-1};
+        }
+
+        int[] answer = new int[arr.length - 1];
         int min = arr[0];
+
         List<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
+        for (int num : arr) {
+            if (num < min) {
+                min = num;
             }
-            list.add(arr[i]);
+            list.add(num);
         }
 
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) == min) {
+            if (min == list.get(i)) {
                 list.remove(i);
             }
         }
 
-        answer = new int[list.size()];
         for (int i = 0; i < answer.length; i++) {
             answer[i] = list.get(i);
-        }
-
-        if (arr.length == 1) {
-            answer = new int[1];
-            answer[0] = -1;
         }
 
         return answer;
@@ -37,6 +38,6 @@ public class DeleteMinNum {
 
     public static void main(String[] args) {
         int[] arr = {4, 3, 2, 1};
-        System.out.println(solution(arr));
+        System.out.println(Arrays.toString(solution(arr)));
     }
 }
