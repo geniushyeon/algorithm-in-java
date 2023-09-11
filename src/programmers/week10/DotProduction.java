500cb9235596dd370e589dc4f6055c16955a8caa
geniushyeon <dev.geniushyeon@gmail.com>
2023-09-11 10:34:25 +0900

    ✨ [programmers] 내적

---
+++ src/programmers/week10/DotProduction.java
@@ -0,0 +1,17 @@
+package programmers.week10;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/70128
+ */
+public class DotProduction {
+
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+
+        return answer;
+    }
+}
