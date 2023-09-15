64607f6ee2835e56fb25f040c56aa3db3c031f94
geniushyeon <dev.geniushyeon@gmail.com>
2023-09-15 23:24:01 +0900

    ✨ [programmers] 가장 가까운 같은 글자

---
+++ src/programmers/week10/NearestCharacter.java
@@ -0,0 +1,41 @@
+package programmers.week10;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/142086
+ */
+public class NearestCharacter {
+
+    public static void main(String[] args) {
+        NearestCharacter nearestCharacter = new NearestCharacter();
+        int[] array = nearestCharacter.solution("banana");
+
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        answer[0] = -1;
+
+        for (int i = 1; i < answer.length; i++) {
+            answer[i] = getIndex(s, i);
+        }
+
+        return answer;
+    }
+
+    private int getIndex(String s, int lastIndex) {
+        int index = lastIndex - 1;
+        int loop = 1;
+
+        while (index >= 0) {
+            if (s.charAt(lastIndex) == s.charAt(index)) {
+                return loop;
+            }
+            loop++;
+            index--;
+        }
+        return -1;
+    }
+}
